d813a56cb093d7dda63f0647dca3eccc54bc10ff
DotNetCore <36761115+johnz22@users.noreply.github.com>
2022-11-01 03:20:17 +1100

    Add files via upload

---
+++ DateOfBirthCalc/Program.cs
@@ -0,0 +1,16 @@
+﻿
+            Console.WriteLine("Welcome To, My First DOB Calc!");
+
+            var Today = DateTime.UtcNow;
+            Console.WriteLine("Type in your D.O.B in DD-MM-YYYY format:");
+
+            var MyBirthday = Console.ReadLine();
+
+            var myBirthdayDate = Convert.ToDateTime(MyBirthday);
+
+            var MyAge = Today.Year - myBirthdayDate.Year;
+
+            if (myBirthdayDate.Date > Today.AddYears(-MyAge)) MyAge--;
+
+            Console.WriteLine($"You are {MyAge} years old");
+            Console.ReadLine();
